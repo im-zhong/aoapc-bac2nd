ea94c6ab3d3e1c9d58bf69c005ff2bfc57e34c69
zhangzhong <im.zhong@outlook.com>
2024-07-26 23:25:51 +0800

    Accepted exercise 3-1

---
+++ src/ch3/exercise_3-1_Score/score.cpp
@@ -0,0 +1,28 @@
+// 2024/7/26
+// zhangzhong
+
+#include <cstdio>
+#include <cstring>
+
+char test[128] = "\0";
+
+int main() {
+    int lines = 0;
+    scanf("%d", &lines);
+
+    for (int _ = 0; _ < lines; _++) {
+        int total_score = 0;
+        int accumulated_score = 0;
+
+        scanf("%s", test);
+        for (char* p = test; *p; p++) {
+            if (*p == 'O') {
+                ++accumulated_score;
+                total_score += accumulated_score;
+            } else {
+                accumulated_score = 0;
+            }
+        }
+        printf("%d\n", total_score);
+    }
+}
