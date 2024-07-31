89d486c5c68fffe7630086e2da4401e9323806c5
zhangzhong <im.zhong@outlook.com>
2024-07-31 21:04:47 +0800

    Accepted Example 4-1 Ancient Cipher

--- src/ch4/Example_4-1_AncientCipher/ancient_cipher.cpp
+++ src/ch4/Example_4-1_AncientCipher/ancient_cipher.cpp
@@ -13,30 +13,36 @@ int str1_chars[26] = {};
 int str2_chars[26] = {};
 
 int main(int argc, char* argv[]) {
-    std::scanf("%s%s", str1, str2);
 
-    for (int i = 0; i < std::strlen(str1); i++) {
-        str1_chars[str1[i] - 'A'] += 1;
-    }
-    for (int i = 0; i < std::strlen(str2); i++) {
-        str2_chars[str2[i] - 'A'] += 1;
-    }
+    while (std::scanf("%s%s", str1, str2) == 2) {
 
-    // 然后我们把两个数组进行排序
-    std::qsort(str1_chars, 26, sizeof(int),
-               [](const void* lhs, const void* rhs) -> int {
-                   return *(int*)(lhs) - *(int*)(rhs);
-               });
-    std::qsort(str2_chars, 26, sizeof(int),
-               [](const void* lhs, const void* rhs) -> int {
-                   return *(int*)(lhs) - *(int*)(rhs);
-               });
-    // then we check if these two array is the same
-    for (int i = 0; i < 26; i++) {
-        if (str1_chars[i] != str2_chars[i]) {
-            std::printf("NO\n");
-            return 0;
+        std::memset(str1_chars, 0, sizeof(str1_chars));
+        std::memset(str2_chars, 0, sizeof(str2_chars));
+
+        for (int i = 0; i < std::strlen(str1); i++) {
+            str1_chars[str1[i] - 'A'] += 1;
         }
+        for (int i = 0; i < std::strlen(str2); i++) {
+            str2_chars[str2[i] - 'A'] += 1;
+        }
+
+        // 然后我们把两个数组进行排序
+        std::qsort(str1_chars, 26, sizeof(int),
+                   [](const void* lhs, const void* rhs) -> int {
+                       return *(int*)(lhs) - *(int*)(rhs);
+                   });
+        std::qsort(str2_chars, 26, sizeof(int),
+                   [](const void* lhs, const void* rhs) -> int {
+                       return *(int*)(lhs) - *(int*)(rhs);
+                   });
+        // then we check if these two array is the same
+        bool is_same = true;
+        for (int i = 0; i < 26; i++) {
+            if (str1_chars[i] != str2_chars[i]) {
+                is_same = false;
+                break;
+            }
+        }
+        std::printf(is_same ? "YES\n" : "NO\n");
     }
-    std::printf("YES\n");
 }
